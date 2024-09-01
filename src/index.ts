b649a74e5b47003603effc1525cab484e4b2fa1b
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-01 06:28:55 +0530

    Updated: moved user crud to routes

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,15 @@
 import { error } from 'console';
 import express from 'express';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 app.use(express.json());
+app.use('/user', userRoutes);
 
 // Home
 app.get('/', (req,res) => {
     res.send('Hello World');
 } );
-
-
-// User CRUD
-
-// Create User
-app.post('/user', (req, res) => {
-    res.status(501).json({error: 'Not implemented'});
-});
-
-// List USers
-app.get('/user', (req, res) => {
-    res.status(501).json({error: 'Not implemented'});
-});
-
-// get one user
-app.get('/user/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
-});
-
-// update user
-app.put('/user/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
-});
-
-// delete user
-app.delete('/user/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
-});
-
-
 
 app.listen(3000, () => {
     console.log("Server ready at localhost:3000");
