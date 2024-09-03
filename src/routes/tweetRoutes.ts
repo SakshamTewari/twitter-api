3a95bc1ac9fee62efa34b77b4f59262d914bace4
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-04 00:43:45 +0530

    Updated: 'Get tweets (GET)'

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = Router();
 const prisma = new PrismaClient();
@@ -8,43 +8,49 @@ const prisma = new PrismaClient();
 
 // Create tweet
 router.post('/', async (req, res) => {
-    const { content, image, userId } = req.body;
-    try {
-      const tweet = await prisma.tweet.create({
-        data: {
-          content,
-          image,
-          userId,  //TODO : manage based on auth user
-        },
-      });
-      res.json(tweet);
-    } catch (e) {
-      const errorMessage = (e as Error).message;
-      res.status(400).json({ error: errorMessage });
-    }
+  const { content, image, userId } = req.body;
+  try {
+    const tweet = await prisma.tweet.create({
+      data: {
+        content,
+        image,
+        userId, //TODO : manage based on auth user
+      },
+    });
+    res.json(tweet);
+  } catch (e) {
+    const errorMessage = (e as Error).message;
+    res.status(400).json({ error: errorMessage });
+  }
 });
 
 // List tweets
-router.get('/', (req, res) => {
-    res.status(501).json({error: 'Not implemented'});
+router.get('/', async (req, res) => {
+  const allTweets = await prisma.tweet.findMany();
+  res.json(allTweets);
 });
 
 // get one tweet
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+
+  if (!tweet) {
+    return res.status(404).json({ error: 'Tweet Not found!!' });
+  }
+  res.json(tweet);
 });
 
 // update tweet
 router.put('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
+  const { id } = req.params;
+  res.status(501).json({ error: `Not implemented: ${id}` });
 });
 
 // delete tweet
 router.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(501).json({error: `Not implemented: ${id}`});
+  const { id } = req.params;
+  res.status(501).json({ error: `Not implemented: ${id}` });
 });
 
 export default router;
