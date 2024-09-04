616f4f4d7d080da4e96bbba3f1970db8f8db559e
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-05 05:00:36 +0530

    Added: 'Update tweet (PUT)'

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -42,9 +42,20 @@ router.get('/:id', async (req, res) => {
 });
 
 // update tweet
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `Not implemented: ${id}` });
+  const { image, content } = req.body;
+
+  try {
+    const result = await prisma.tweet.update({
+      where: { id: Number(id) },
+      data: { image, content },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ error: 'Could not update the tweet' });
+  }
 });
 
 // delete tweet
