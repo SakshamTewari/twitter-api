e513a643b8047aaa118b01fbd9ba70f24f403d6a
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-07 23:09:59 +0530

    Updated with authRoute

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { error } from 'console';
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import tweetRoutes from './routes/tweetRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
 app.use(express.json());
@@ -12,11 +13,14 @@ app.use('/user', userRoutes);
 // Tweet route
 app.use('/tweet', tweetRoutes);
 
+// Auth route
+app.use('/auth', authRoutes);
+
 // Home
-app.get('/', (req,res) => {
-    res.send('Hello World');
-} );
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.listen(3000, () => {
-    console.log("Server ready at localhost:3000");
-})
+  console.log('Server ready at localhost:3000');
+});
