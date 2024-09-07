c5f99be01e82b66a77d6fa688ef0edbc0b18e535
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-07 23:19:43 +0530

    try-catch

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -20,24 +20,31 @@ router.post('/login', async (req, res) => {
     new Date().getTime() + EMAIL_TOKEN_EXPIRATION_MINUTES * 60 * 1000,
   ); // convert EMAIL_TOKEN_EXPIRATION_MINUTES to milliseconds as getTime() is in ms
 
-  const createdToken = await prisma.token.create({
-    data: {
-      type: 'EMAIL',
-      emailToken,
-      expiration,
-      // now to connect with userId, we have only 'email' to do a listLookup
-      // If a user with the provided email already exists: Prisma will return the existing user and associate that user with the created token.
-      // The existing user's id will be included as part of the returned data.
-      // If no user with the provided email exists: Prisma will create a new user with the specified email, and the newly created user's id will be returned as part of the returned data.
-      // In both cases, the user field in the createdToken will be linked to a user, and the resulting token data will contain the user's information, including the id.
-      user: {
-        connectOrCreate: {
-          where: { email },
-          create: { email },
+  try {
+    const createdToken = await prisma.token.create({
+      data: {
+        type: 'EMAIL',
+        emailToken,
+        expiration,
+        // now to connect with userId, we have only 'email' to do a listLookup
+        // If a user with the provided email already exists: Prisma will return the existing user and associate that user with the created token.
+        // The existing user's id will be included as part of the returned data.
+        // If no user with the provided email exists: Prisma will create a new user with the specified email, and the newly created user's id will be returned as part of the returned data.
+        // In both cases, the user field in the createdToken will be linked to a user, and the resulting token data will contain the user's information, including the id.
+        user: {
+          connectOrCreate: {
+            where: { email },
+            create: { email },
+          },
         },
       },
-    },
-  });
+    });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(400)
+      .json({ error: "Couldn't start the authentication process" });
+  }
 
   // send emailToken to user's email
 });
